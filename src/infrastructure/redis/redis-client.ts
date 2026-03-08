cd7cb5aa748873beb12e93aa00149463b7342888
kunalyeola <kunalyeola1999@gmail.com>
2026-03-08 15:37:02 +0530

    initial

--- src/infrastructure/redis/redis-client.ts
+++ src/infrastructure/redis/redis-client.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const redisClient: RedisClientType = createClient({
-  url: `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`
+  url: process.env.REDIS_URL || `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`
 });
 
 redisClient.on('error', (err) => logger.error('Redis Client Error', err));
